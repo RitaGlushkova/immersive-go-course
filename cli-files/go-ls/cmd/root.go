5b3e7320d738399c3635377fe17d45376a608b76
Margarita Glushkova <m.glushkova1@gmail.com>
2022-09-09 16:21:10 +0100

    go-cat implemented, go-ls basic functionality
    GO-LS: Only basic functionality, No tests, no flags, no help yet
    GO-CAT: implementation done, may require further refactoring

--- cli-files/go-ls/cmd/root.go
+++ cli-files/go-ls/cmd/root.go
@@ -1,7 +1,44 @@
 package cmd
-import (
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
 )
 
+func NewRootCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "go-ls",
+		Short: "go-ls command reads a directory, generating a list of files or sub-directories",
+		Long:  ``,
+		Args:  cobra.ArbitraryArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			dir := "."
+			if len(args) > 0 {
+				dir = args[0]
+			}
+			if _, err := os.Stat(dir); err == nil {
+				fmt.Printf("%s\n", args[0])
+				return err
+			}
+			files, err := os.ReadDir(args[0])
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, file := range files {
+				fmt.Println(file.Name())
+			}
+			return nil
+		},
+	}
+}
+
 func Execute() {
+	rootCmd := NewRootCmd()
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
--- http-auth/main.go
+++ http-auth/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"log"
 	"net/http"
-	 "fmt"
-	 "strings"
+	"strings"
 )
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world"))
-	})
+		switch r.Method {
+		case "GET": 
+			w.Header().Set("Content-Type", "text/html") //text/plain sends a string as response
+			w.Write([]byte("<!DOCTYPE html><html><em>Hello, world</em>\n"))
+		case "POST": 
+		w.Header().Set("Content-Type", "text/html")
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Write([]byte(b))
+		default:
+		w.Write([]byte("Only GET and POST methods are available"))
+	}})
+	
 	http.HandleFunc("/200", func(w http.ResponseWriter, r *http.Request) {
 		prefix := "/"
 		w.Write([]byte(fmt.Sprintf("%v\n", strings.TrimPrefix(r.URL.Path, prefix))))
