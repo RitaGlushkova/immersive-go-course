3f28c28994bf578fb076c8705bc1f8136396cf7f
Margarita Glushkova <m.glushkova1@gmail.com>
2022-09-28 22:20:25 +0100

    make initial functionality work
    completed initial assignment
    still need to do tests
    still need to do stretch goals
    need extra work on handling errors

--- server-database/main.go
+++ server-database/main.go
@@ -1,46 +1,126 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/jackc/pgx/v4"
 )
 
 type Image struct {
 	Title   string `json:"title"`
-	AltText string `json:"alt_text"`
 	URL     string `json:"url"`
+	AltText string `json:"alt_text"`
+}
+
+func encodedMarshalJSON(data interface{}, queryVal string) ([]byte, error) {
+	indent, errIndent := strconv.Atoi(queryVal)
+	var marshalData []byte
+	var marshalErr error
+	if errIndent != nil {
+		//DO I WANT TO INFORM ABOUT IT ????
+	}
+	if indent > 0 && errIndent == nil {
+		marshalData, marshalErr = json.MarshalIndent(data, "", strings.Repeat(" ", indent))
+	} else {
+		marshalData, marshalErr = json.Marshal(data)
+	}
+	return marshalData, marshalErr
+}
+
+func fetchImages(conn *pgx.Conn) ([]Image, error) {
+	rows, err := conn.Query(context.Background(), "SELECT title, url, alt_text FROM public.images")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Query failed: %v\n", err)
+		os.Exit(1)
+	}
+	var images []Image
+	for rows.Next() {
+		var title, url, altText string
+		err = rows.Scan(&title, &url, &altText)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Scan for raw failed: %v\n", err)
+			os.Exit(1)
+		}
+		images = append(images, Image{Title: title, URL: url, AltText: altText})
+	}
+	return images, err
+}
+
+func postImage(conn *pgx.Conn, r *http.Request) (*Image, error) {
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't read body: %v\n", err)
+		os.Exit(1)
+	}
+	var img Image
+	err = json.Unmarshal(b, &img)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't decode input: %v\n", err)
+		os.Exit(1)
+	}
+	_, err = conn.Exec(context.Background(), "INSERT INTO public.images (title, url, alt_text) VALUES ($1, $2, $3)", img.Title, img.URL, img.AltText)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "POST query failed: %v\n", err)
+		os.Exit(1)
+	}
+	return &img, err
 }
 
 func handlerImages(w http.ResponseWriter, r *http.Request) {
-	images := []Image{
-		{
-			Title:   "Sunset",
-			AltText: "Clouds at sunset",
-			URL:     "https://images.unsplash.com/photo-1506815444479-bfdb1e96c566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
-		},
-		{
-			Title:   "Mountain",
-			AltText: "A mountain at sunset",
-			URL:     "https://images.unsplash.com/photo-1540979388789-6cee28a1cdc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
-		},
+	//not sure it is a good solution
+	_, ok := os.LookupEnv("DATABASE_URL")
+	if !ok {
+		fmt.Fprintf(os.Stderr, "Environment variable is not set")
+		os.Exit(1)
 	}
-	var indentation = " "
-	values, ok := r.URL.Query()["intent"]
-	if ok {
-		if values[0] == "2" {
-			indentation = "  "
-		}
-		if values[0] == "4" {
-			indentation = "    "
-		}
-	}
-	b, err := json.MarshalIndent(images, "", indentation)
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
-		log.Print(err)
-		w.WriteHeader(500)
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
 	}
-	w.Write([]byte(b))
+	defer conn.Close(context.Background())
+	queryVal := r.URL.Query().Get("indent")
+
+	switch r.Method {
+	case "GET":
+		images, err := fetchImages(conn)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Couldn't fetch: %v\n", err)
+			os.Exit(1)
+		}
+		encoded, err := encodedMarshalJSON(images, queryVal)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Couldn't encode correctly: %v\n", err)
+			os.Exit(1)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(encoded))
+	case "POST":
+		img, err := postImage(conn, r)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Print(err)
+		}
+		encoded, err := encodedMarshalJSON(img, queryVal)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Couldn't encode inserted values: %v\n", err)
+			os.Exit(1)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(encoded))
+	default:
+		w.WriteHeader(405)
+		w.Write([]byte("Only GET and POST methods are available"))
+
+	}
 }
 
 func main() {
