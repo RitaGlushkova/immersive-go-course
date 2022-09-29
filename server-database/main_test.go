8e0dadd6cf4e216f7f53f5508151a42a39ce123f
Margarita Glushkova <m.glushkova1@gmail.com>
2022-09-29 23:00:46 +0100

    test POST request with Query

--- server-database/main_test.go
+++ server-database/main_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	//"bytes"
+	"bytes"
 	"context"
+
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/jackc/pgx/v4"
@@ -28,7 +32,9 @@ func setupSuite(tb testing.TB) func(tb testing.TB) {
 	conn, err := pgx.Connect(context.Background(), TEST_DB_URL)
 	if err != nil {
 		tb.Fatalf("Teardown Error: Unable to connect to DB: %s", err.Error())
+		os.Exit(1)
 	}
+
 	_, err = conn.Exec(context.Background(), `INSERT INTO public.images (title, url, alt_text) VALUES ('White Cat','https://images.freeimages.com/images/previews/13e/my-cat-1363423.jpg','White cat sitting and looking to the left'),('Catch a Ball','https://images.freeimages.com/images/large-previews/12a/dog-1361473.jpg','A dog jumping up catching a red ball')`)
 	if err != nil {
 		tb.Fatalf("Teardown Error: Unable to insert data: %s", err.Error())
@@ -36,7 +42,7 @@ func setupSuite(tb testing.TB) func(tb testing.TB) {
 
 	return func(tb testing.TB) {
 		// teardown the database after testing
-		_, err := conn.Exec(context.Background(), "DELETE from images")
+		_, err := conn.Exec(context.Background(), "DELETE from public.images")
 
 		if err != nil {
 			tb.Fatalf("Teardown Error: Unable to delete from images: %s", err.Error())
@@ -49,17 +55,17 @@ func TestMain(t *testing.T) {
 	defer teardownSuite(t)
 	conn, err := pgx.Connect(context.Background(), TEST_DB_URL)
 	if err != nil {
-		t.Error(err)
+		t.Fatalf("Unable to insert data: %s", err.Error())
 	}
 	//require.NoError(t, err)
 	s := &Server{conn: conn}
-	t.Run("GET /", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/", nil)
-		response := httptest.NewRecorder()
-		handlerIndex(response, request)
-		assertStatus(t, response.Code, http.StatusOK)
-		require.Equal(t, response.Body.String(), "Hello World")
-	})
+	// t.Run("GET /", func(t *testing.T) {
+	// 	request, _ := http.NewRequest(http.MethodGet, "/", nil)
+	// 	response := httptest.NewRecorder()
+	// 	handlerIndex(response, request)
+	// 	assertStatus(t, response.Code, http.StatusOK)
+	// 	require.Equal(t, response.Body.String(), "Hello World")
+	// })
 	t.Run("GET /images.json (WITHOUT QUERY)", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodGet, "/images.json", nil)
 		response := httptest.NewRecorder()
@@ -93,19 +99,23 @@ func TestMain(t *testing.T) {
 		assertStatus(t, response.Code, http.StatusOK)
 		require.Equal(t, want, response.Body.String())
 	})
-	// t.Run("POST /", func(t *testing.T) {
-	// 	b := strings.NewReader("<em>Hi</em>")
-	// 	request, _ := http.NewRequest(http.MethodPost, "/", b)
-	// 	response := httptest.NewRecorder()
-	// 	handlerIndex(response, request)
-	// 	assertStatus(t, response.Code, http.StatusAccepted)
-	// 	assertResponseBody(t, response.Body.String(), "<!DOCTYPE html><html>&lt;em&gt;Hi&lt;/em&gt;\n")
-	// })
 
-	// Test PostImage
-	// Test Main
-	// Test DB connection
-	// Test encoding
+	t.Run("POST /images.json (WITH QUERY)", func(t *testing.T) {
+		input := []byte(`{"title": "Big Ben", "url": "https://images.freeimages.com/images/large-previews/3d0/london-1452422.jpg","alt_text": "Big Ben and Tube sign"}`)
+		request, _ := http.NewRequest(http.MethodPost, "/images.json", bytes.NewBuffer(input))
+		q := request.URL.Query()
+		q.Add("indent", "2")
+		request.URL.RawQuery = q.Encode()
+		response := httptest.NewRecorder()
+		s.handlerImages(response, request)
+		want := `{
+  "title": "Big Ben",
+  "url": "https://images.freeimages.com/images/large-previews/3d0/london-1452422.jpg",
+  "alt_text": "Big Ben and Tube sign"
+}`
+		require.Equal(t, want, response.Body.String())
+
+	})
 }
 
 func assertStatus(t testing.TB, got, want int) {
