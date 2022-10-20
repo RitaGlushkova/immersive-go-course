0c083d9f89a942eaeb3896a00953497a7fab87fa
Margarita Glushkova <93099190+RitaGlushkova@users.noreply.github.com>
2022-10-20 17:04:09 +0100

    create and output.csv and write some stuff in it

--- batch-processing/helpers.go
+++ batch-processing/helpers.go
@@ -3,58 +3,111 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
+	"path/filepath"
+
+	"gopkg.in/gographics/imagick.v2/imagick"
 )
 
+type Row struct {
+	url string
+	title string
+}
 
-func ReadCsvFile(filename string) ([][]string, error) {
-   // Open CSV file
+type ConvertImageCommand func(args []string) (*imagick.ImageCommandResult, error)
+
+type Converter struct {
+	cmd ConvertImageCommand
+}
+
+func (c *Converter) Grayscale(inputFilepath string, outputFilepath string) error {
+	// Convert the image to grayscale using imagemagick
+	// We are directly calling the convert command
+	_, err := c.cmd([]string{
+		"convert", inputFilepath, "-set", "colorspace", "Gray", outputFilepath,
+	})
+	return err
+}
+
+func ReadCsvFile(filename, headerTitle string) ([][]string, error) {
+   // Open CSV file from there to read url links
    fileContent, err := os.Open(filename)
    if err != nil {
       return [][]string{}, err
    }
    defer fileContent.Close()
 
-   // Read File into a Variable
+   //read from this file
    records, err := csv.NewReader(fileContent).ReadAll()
-
    if err != nil {
       return [][]string{}, err
    }
 
+   //check if there is any content
    if len(records) == 0 {
 	return [][]string{}, fmt.Errorf("empty csv")
    }
 
+   //check if header and thus info is what we expect
    header := records[0]
-
-   if header[0] != "url" || len(header) == 0 {
-	return [][]string{}, fmt.Errorf("incorrect header %s, url is required", header[0])
+   if header[0] != headerTitle || len(header) == 0 {
+	return [][]string{}, fmt.Errorf("incorrect header, expected %s, got %s", headerTitle, header[0])
    }
 
+   //if no errors return records (first will be title)
    return records, nil
 }
 
-// func downloadAnImage(url string, name string) error{
-//     r, err := http.Get(url)
-//     if err != nil {
-//         return fmt.Errorf("couldn't process get request from %s. Error: %v", url, err)
-//     }
-//     defer r.Body.Close()
+func (row *Row) DownloadAndSaveImage(url, name, inputPath, outputPath string) error{
 
-//     fname := name+".jpg"
-// 	//create file where to download the image
-//     f, err := os.Create(fname)
-//     if err != nil {
-//         return err
-//     }
-//     defer f.Close()
+	//make GET request to URL
+    r, err := http.Get(url)
+    if err != nil {
+        return fmt.Errorf("couldn't process get request from %s. Error: %v", url, err)
+    }
+    defer r.Body.Close()
 
-//     _, err = io.Copy(fname, r.Body)
+	//create file where to download content of url
+	fileName := fmt.Sprintf("%s.jpg", name)
+	inputFilepath := filepath.Join(inputPath, fileName)
+	outputFilepath := filepath.Join(outputPath, fileName)
+	file, err := os.Create(inputFilepath)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
 
-//     if err != nil {
-//         return err
-// }
-// fmt.Println("image downloaded")
-// 	return nil
-// }
+	//copy content from URl to the file
+    _, err = io.Copy(file, r.Body)
+    if err != nil {
+        return err
+	}
+
+	// Set up imagemagick
+	imagick.Initialize()
+	defer imagick.Terminate()
+
+	 // Log what we're going to do
+	 log.Printf("processing: %q to %q\n", inputFilepath, outputFilepath)
+
+	// Build a Converter struct that will use imagick
+	c := &Converter{
+		cmd: imagick.ConvertImageCommand,
+	}
+
+	// Do the conversion! and save to output folder
+	err = c.Grayscale(inputFilepath, outputFilepath)
+	if err != nil {
+		log.Printf("error: %v\n", err)
+	}
+
+	 // Log what we did
+ 	log.Printf("processed: %q to %q\n", inputFilepath, outputFilepath)
+
+	return nil		
+
+}
+
--- batch-processing/main.go
+++ batch-processing/main.go
@@ -1,124 +1,56 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os"
 	"path/filepath"
-
-	"gopkg.in/gographics/imagick.v2/imagick"
 )
-
-type ConvertImageCommand func(args []string) (*imagick.ImageCommandResult, error)
-
-type Converter struct {
-	cmd ConvertImageCommand
-}
-
-func (c *Converter) Grayscale(inputFilepath string, outputFilepath string) error {
-	// Convert the image to grayscale using imagemagick
-	// We are directly calling the convert command
-	_, err := c.cmd([]string{
-		"convert", inputFilepath, "-set", "colorspace", "Gray", outputFilepath,
-	})
-	return err
-}
-
-
-func DownloadAnImage(url, name, inputFilepath, outputFilepath string) error{
-    r, err := http.Get(url)
-    if err != nil {
-        return fmt.Errorf("couldn't process get request from %s. Error: %v", url, err)
-    }
-    defer r.Body.Close()
-
-	//create file where to download the image
-	fname := fmt.Sprintf("%s.jpg", name)
-	file, err := os.Create(filepath.Join(inputFilepath, fname))
-    if err != nil {
-        return err
-    }
-    defer file.Close()
-    _, err = io.Copy(file, r.Body)
-    if err != nil {
-        return err
-}
-
-	// Set up imagemagick
-	imagick.Initialize()
-	defer imagick.Terminate()
-
-	 // Log what we're going to do
-	 log.Printf("processing: %q to %q\n", inputFilepath, outputFilepath)
-
-	// Build a Converter struct that will use imagick
-	c := &Converter{
-		cmd: imagick.ConvertImageCommand,
-	}
-
-	// Do the conversion!
-	err = c.Grayscale(filepath.Join(inputFilepath, fname), filepath.Join(outputFilepath, fname))
-	if err != nil {
-		log.Printf("error: %v\n", err)
-	}
-
-	 // Log what we did
- 	log.Printf("processed: %q to %q\n", inputFilepath, outputFilepath)
-	fmt.Println("image downloaded")
-	return nil		
-
-}
-
-
 
 
 func main() {
 
 	// Accept --input and --output arguments for the images
-	inputFilepath := flag.String("input", "", "A path to an image to be processed")
-	outputFilepath := flag.String("output", "", "A path to where the processed image should be written")
+	inputPath := flag.String("input", "", "A path to an image to be processed")
+	outputPath := flag.String("output", "", "A path to where the processed image should be written")
 	flag.Parse()
 
 	// Ensure that both flags were set
-	if *inputFilepath == "" || *outputFilepath == "" {
+	if *inputPath == "" || *outputPath == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	records, err := ReadCsvFile("/inputs/input.csv")
+	records, err := ReadCsvFile("/inputs/input.csv", "url")
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	outputRecords := make([][]string, len(records)-1)
+	outputRecords = append(outputRecords, []string{"url", "title"})
 	records = records[1:]
 	for i, record := range records {
-		err := DownloadAnImage(record[0], fmt.Sprint(i), *inputFilepath, *outputFilepath)
+
+		row := Row {
+			url: record[0],
+			title: fmt.Sprintf("%s.%s", fmt.Sprint(i), "jpg"),
+		}
+		err := row.DownloadAndSaveImage(record[0], fmt.Sprint(i), *inputPath, *outputPath)
 		if err != nil {
 			log.Fatal(err)
 		}
+		outputRecords = append(outputRecords, []string{row.url, row.title})
 	}
-// // Set up imagemagick
-// 	imagick.Initialize()
-// 	defer imagick.Terminate()
-
-// 	 // Log what we're going to do
-// 	 log.Printf("processing: %q to %q\n", *inputFilepath, *outputFilepath)
-
-// 	// Build a Converter struct that will use imagick
-// 	c := &Converter{
-// 		cmd: imagick.ConvertImageCommand,
-// 	}
-
-// 	// Do the conversion!
-// 	err = c.Grayscale(*inputFilepath, *outputFilepath)
-// 	if err != nil {
-// 		log.Printf("error: %v\n", err)
-// 	}
-
-// 	 // Log what we did
-//  log.Printf("processed: %q to %q\n", *inputFilepath, *outputFilepath)
-	
+	csvFile, err := os.Create(filepath.Join(*outputPath, "output.csv"))
+	if err != nil {
+    log.Fatalf("failed creating file: %s", err)
+	}
+	defer csvFile.Close()
+	w := csv.NewWriter(csvFile)
+    defer w.Flush()
+	err = w.WriteAll(outputRecords)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
