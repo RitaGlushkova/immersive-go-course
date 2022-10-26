119be7b06b37022e007c99fa7fdac89448fc9c17
Margarita Glushkova <93099190+RitaGlushkova@users.noreply.github.com>
2022-10-26 14:10:30 +0100

    error chan not working

--- batch-processing/helpers.go
+++ batch-processing/helpers.go
@@ -16,22 +16,21 @@ import (
 )
 
 type Row struct {
-	url string
-	input string
+	url    string
+	input  string
 	output string
 }
 
-
 type ProcessDownloadImage struct {
-	url string
-	filePath string
-	err error
+	url   string
+	input string
+	err   error
 }
 type ProcessUploadImage struct {
-	url string
-	inputPath string
-	outputPath string
-	err error
+	url    string
+	input  string
+	output string
+	err    error
 }
 
 type ConvertImageCommand func(args []string) (*imagick.ImageCommandResult, error)
@@ -44,7 +43,6 @@ func genFilepath(suffix string) string {
 	return fmt.Sprintf("%d-%d.%s", time.Now().UnixMilli(), rand.Int(), suffix)
 }
 
-
 func (c *Converter) Grayscale(inputFilepath string, outputFilepath string) error {
 	// Convert the image to grayscale using imagemagick
 	// We are directly calling the convert command
@@ -55,62 +53,56 @@ func (c *Converter) Grayscale(inputFilepath string, outputFilepath string) error
 }
 
 func ReadCsvFile(filename, headerTitle string) ([][]string, error) {
-   // Open CSV file from there to read url links
-   fileContent, err := os.Open(filename)
-   if err != nil {
-      return [][]string{}, err
-   }
-   defer fileContent.Close()
+	// Open CSV file from there to read url links
+	fileContent, err := os.Open(filename)
+	if err != nil {
+		return [][]string{}, err
+	}
+	defer fileContent.Close()
 
-   //read from this file
-   records, err := csv.NewReader(fileContent).ReadAll()
-   if err != nil {
-      return [][]string{}, err
-   }
+	//read from this file
+	records, err := csv.NewReader(fileContent).ReadAll()
+	if err != nil {
+		return [][]string{}, err
+	}
 
-   //check if there is any content
-   if len(records) == 0 {
-	return [][]string{}, fmt.Errorf("empty csv")
-   }
+	//check if there is any content
+	if len(records) == 0 {
+		return [][]string{}, fmt.Errorf("empty csv")
+	}
 
-   //check if header and thus info is what we expect
-   header := records[0]
-   if header[0] != headerTitle || len(header) == 0 {
-	return [][]string{}, fmt.Errorf("incorrect header, expected %s, got %s", headerTitle, header[0])
-   }
-   return records, nil
+	//check if header and thus info is what we expect
+	header := records[0]
+	if header[0] != headerTitle || len(header) == 0 {
+		return [][]string{}, fmt.Errorf("incorrect header, expected %s, got %s", headerTitle, header[0])
+	}
+	fmt.Println("read a record")
+	return records, nil
 }
 
-func DownloadImageS(urlsChan chan string, inputPathsChan chan ProcessDownloadImage, inputPath string) {
+func DownloadImageS(urlsChan chan string, inputPathsChan chan ProcessDownloadImage, inputPath string, processingErrorChan chan error, wg *sync.WaitGroup) {
 	for url := range urlsChan {
-	d := DownloadImage(url, inputPath)
-	if d.err != nil {
-		fmt.Println(d.err, "ERROR IN DOWNLOAD")}
-		inputPathsChan <- d
-}
-}
-
-func ConvertImages(inputPathsChan chan ProcessDownloadImage, outputPath string, outputPathsChan chan ProcessUploadImage) {
-	for inputPath := range inputPathsChan {
-		conv := ConvertImageIntoGreyScale(inputPath.filePath, outputPath, inputPath.url) 
-		if conv.err != nil{
-			fmt.Println(conv.err, "ERROR IN CONVERT")
+		d := DownloadImage(url, inputPath)
+		if d.err != nil {
+			processingErrorChan <- d.err
+			fmt.Println(d.err)
+			wg.Done()
 		}
-			outputPathsChan <- conv
-		
+		inputPathsChan <- d
 	}
 }
 
-func WriteIntoOutputSlice(outputPathsChan chan ProcessUploadImage, outputWriteChan chan Row, wg *sync.WaitGroup) {
-	for outputProcessedImage := range outputPathsChan {
-		row := Row {
-			url: outputProcessedImage.url,
-			input: outputProcessedImage.inputPath,
-			output: outputProcessedImage.outputPath,
+func ConvertImages(inputPathsChan chan ProcessDownloadImage, outputPath string, outputPathsChan chan ProcessUploadImage, processingErrorChan chan error, wg *sync.WaitGroup) {
+	for inputPath := range inputPathsChan {
+		conv := ConvertImageIntoGreyScale(inputPath.input, outputPath, inputPath.url)
+		if conv.err != nil {
+			processingErrorChan <- conv.err
+			fmt.Println(conv.err)
+			wg.Done()
 		}
-		outputWriteChan <- row
-	wg.Done()
-	}	
+		outputPathsChan <- conv
+		wg.Done()
+	}
 }
 
 func DownloadImage(url, inputPath string) ProcessDownloadImage {
@@ -119,38 +111,38 @@ func DownloadImage(url, inputPath string) ProcessDownloadImage {
 		fmt.Printf("downloaded file in %s\n", time.Since(start))
 	}()
 	//make GET request to URL
-    r, err := http.Get(url)
-    if err != nil {
-        return ProcessDownloadImage{url: url, filePath: "", err:fmt.Errorf("couldn't process get request from %s. Error: %v", url, err)}
-    }
-    defer r.Body.Close()
+	r, err := http.Get(url)
+	if err != nil {
+		return ProcessDownloadImage{url: url, input: "", err: fmt.Errorf("couldn't process get request from %s. Error: %v", url, err)}
+	}
+	defer r.Body.Close()
 
 	//create file where to download content of url
 	inputFilepath := filepath.Join(inputPath, genFilepath("jpg"))
 
 	// need to change to temp directory
 	file, err := os.Create(inputFilepath)
-    if err != nil {
-        return ProcessDownloadImage{url: url, filePath: "", err:fmt.Errorf("FILE NOT CREATED: %v", err)}
-    }
-    defer file.Close()
+	if err != nil {
+		return ProcessDownloadImage{url: url, input: "", err: fmt.Errorf("file could not be created: %v", err)}
+	}
+	defer file.Close()
 
 	//copy content from URl to the file
-    _, err = io.Copy(file, r.Body)
-    if err != nil {
-        return ProcessDownloadImage{url: url, filePath: "", err:fmt.Errorf("DATA NOT COPIED INTO A FILE: %v", err)}
+	_, err = io.Copy(file, r.Body)
+	if err != nil {
+		return ProcessDownloadImage{url: url, input: "", err: fmt.Errorf("data not copied into a file: %v", err)}
 	}
 
-	return ProcessDownloadImage{url: url, filePath:inputFilepath, err:nil}	
+	return ProcessDownloadImage{url: url, input: inputFilepath, err: nil}
 }
 
 func ConvertImageIntoGreyScale(inputFilepath, outputPath string, url string) ProcessUploadImage {
-// Set up imagemagick
+	// Set up imagemagick
 	imagick.Initialize()
 	defer imagick.Terminate()
 	outputFilepath := filepath.Join(outputPath, genFilepath("jpg"))
-	 // Log what we're going to do
-	 log.Printf("processing: %q to %q\n", inputFilepath, outputFilepath)
+	// Log what we're going to do
+	log.Printf("processing: %q to %q\n", inputFilepath, outputFilepath)
 
 	// Build a Converter struct that will use imagick
 	c := &Converter{
@@ -160,9 +152,9 @@ func ConvertImageIntoGreyScale(inputFilepath, outputPath string, url string) Pro
 	// Do the conversion! and save to output folder
 	err := c.Grayscale(inputFilepath, outputFilepath)
 	if err != nil {
-	return ProcessUploadImage{url: url, inputPath:inputFilepath, outputPath: "", err:fmt.Errorf("error: %v\n", err)}	
+		return ProcessUploadImage{url: url, input: inputFilepath, output: "", err: fmt.Errorf("error: %v\n", err)}
 	}
-	 // Log what we did
- 	log.Printf("processed: %q to %q\n", inputFilepath, outputFilepath)
-	return  ProcessUploadImage{url: url, inputPath:inputFilepath, outputPath: outputFilepath, err:nil}	
+	// Log what we did
+	log.Printf("processed: %q to %q\n", inputFilepath, outputFilepath)
+	return ProcessUploadImage{url: url, input: inputFilepath, output: outputFilepath, err: nil}
 }
--- batch-processing/main.go
+++ batch-processing/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/csv"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
@@ -10,7 +11,7 @@ import (
 )
 
 func main() {
-	
+
 	// Accept --input and --output arguments for the images
 	inputPath := flag.String("input", "", "A path to an image to be processed")
 	outputPath := flag.String("output", "", "A path to where the processed image should be written")
@@ -21,46 +22,45 @@ func main() {
 		flag.Usage()
 		os.Exit(1)
 	}
-
-	records, err := ReadCsvFile("/inputs/input.csv", "url")
+	records, err := ReadCsvFile(filepath.Join(*inputPath, "input.csv"), "url")
+	//fmt.Println(len(records))
 	if err != nil {
 		log.Fatal(err)
 	}
-	outputRecords := make([][]string, len(records)-1)
+	outputRecords := make([][]string, len(records))
 	outputRecords = append(outputRecords, []string{"url", "input", "output"})
 	records = records[1:]
 	var wg sync.WaitGroup
-	urlsChan := make(chan string, 3)
-	inputPathsChan := make(chan ProcessDownloadImage, 3)
+	urlsChan := make(chan string)
+	processingErrorChan := make(chan error)
+	inputPathsChan := make(chan ProcessDownloadImage)
 	outputPathsChan := make(chan ProcessUploadImage)
-	outputWriteChan := make(chan Row)
-	for i:=0; i<3; i++ {
-	go DownloadImageS(urlsChan, inputPathsChan, *inputPath)
-	}
-	go ConvertImages(inputPathsChan, *outputPath, outputPathsChan)
-	go WriteIntoOutputSlice(outputPathsChan, outputWriteChan, &wg)
+	//for i:=0; i<3; i++ {
+	go DownloadImageS(urlsChan, inputPathsChan, *inputPath, processingErrorChan, &wg)
+	//}
+	go ConvertImages(inputPathsChan, *outputPath, outputPathsChan, processingErrorChan, &wg)
 
 	for _, record := range records {
 		wg.Add(1)
 		urlsChan <- record[0]
-    }
-    
-	close(urlsChan)
+	}
 	wg.Wait()
 
-	for row := range outputWriteChan {
+	select {
+	case err := <-processingErrorChan:
+		fmt.Println(err)
+	case row := <-outputPathsChan:
 		outputRecords = append(outputRecords, []string{row.url, row.input, row.output})
-    }
+	}
 
 	csvFile, err := os.Create(filepath.Join(*outputPath, "output.csv"))
 	if err != nil {
-    log.Fatalf("failed creating file: %s", err)
+		log.Fatalf("failed creating file: %s", err)
 	}
 	defer csvFile.Close()
 	w := csv.NewWriter(csvFile)
-    defer w.Flush()
 	err = w.WriteAll(outputRecords)
 	if err != nil {
 		log.Fatal(err)
-}
+	}
 }
