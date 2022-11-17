2b76741567f3cfd1465de9d872f5e8e1fc2d75d8
Margarita Glushkova <93099190+RitaGlushkova@users.noreply.github.com>
2022-11-17 13:11:40 +0000

    note by id bug

--- buggy-app/api/model/notes.go
+++ buggy-app/api/model/notes.go
@@ -54,18 +54,18 @@ func GetNotesForOwner(ctx context.Context, conn dbConn, owner string) (Notes, er
 	if queryRows.Err() != nil {
 		return nil, fmt.Errorf("model: query read failed: %w", queryRows.Err())
 	}
-
+	fmt.Println(len(notes))
 	return notes, nil
 }
 
-func GetNoteById(ctx context.Context, conn dbConn, id string) (Note, error) {
+func GetNoteById(ctx context.Context, conn dbConn, id string, owner string) (Note, error) {
 	var note Note
 	if id == "" {
 		return note, errors.New("model: id not supplied")
 	}
 
-	row := conn.QueryRow(ctx, "SELECT id, owner, content, created, modified FROM public.note WHERE id = $1", id)
-
+	row := conn.QueryRow(ctx, "SELECT id, owner, content, created, modified FROM public.note WHERE id = $1 AND owner = $2", id, owner)
+	fmt.Println(row)
 	err := row.Scan(&note.Id, &note.Owner, &note.Content, &note.Created, &note.Modified)
 	if err != nil {
 		return note, fmt.Errorf("model: query scan failed: %w", err)
