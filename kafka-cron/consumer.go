c883489954bf59abaf9ea438283c04cb9640f453
Margarita Glushkova <93099190+RitaGlushkova@users.noreply.github.com>
2022-12-06 14:56:24 +0000

    first commit

---
+++ kafka-cron/consumer.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "host1:9092,host2:9092",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "smallest"})
+	if err != nil {
+		panic(err)
+	}
+
+	consumer.SubscribeTopics([]string{"myTopic", "^aRegex.*[Tt]opic"}, nil)
+	run := true
+	for run == true {
+		ev := consumer.Poll(100)
+		switch e := ev.(type) {
+		case *kafka.Message:
+			// application-specific processing
+		case kafka.Error:
+			fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
+			run = false
+		default:
+			fmt.Printf("Ignored %v\n", e)
+		}
+	}
+	consumer.Close()
+}
---
+++ kafka-cron/go.mod
@@ -0,0 +1,10 @@
+module kafka
+
+go 1.19
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/confluentinc/examples v7.3.0+incompatible
+	github.com/google/uuid v1.3.0
+	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.8.2
+)
