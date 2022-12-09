3be7e1f09a76c5ca4cf201e1c96e38f12d370ccb
Margarita Glushkova <93099190+RitaGlushkova@users.noreply.github.com>
2022-12-09 11:54:44 +0000

    add flags

---
+++ kafka-cron/consumer/consumer.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+)
+
+type cronjob struct {
+	Crontab string   `json:"crontab"`
+	Command string   `json:"command"`
+	Args    []string `json:"args"`
+	// cluster string
+	// retries int
+}
+
+const (
+	DefaultKafkaTopic    = "test_topic"
+	DefaultConsumerGroup = "myGroup"
+)
+
+var (
+	consumerGroup = flag.String("group", DefaultConsumerGroup, "The name of the consumer group, used for coordination and load balancing")
+	kafkaTopic    = flag.String("topic", DefaultKafkaTopic, "The comma-separated list of topics to consume")
+)
+
+func main() {
+	flag.Parse()
+
+	// Store the config
+	cm := kafka.ConfigMap{
+		"bootstrap.servers":  "localhost:9092",
+		"group.id":           *consumerGroup,
+		"session.timeout.ms": 6000,
+		"auto.offset.reset":  "earliest",
+	}
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+	// Variable p holds the new Consumer instance.
+	c, err := kafka.NewConsumer(&cm)
+	// Check for errors in creating the Consumer
+	if err != nil {
+		if ke, ok := err.(kafka.Error); ok {
+			switch ec := ke.Code(); ec {
+			case kafka.ErrInvalidArg:
+				fmt.Printf("Can't create the Consumer because you've configured it wrong (code: %d)!\n\t%v\n", ec, err)
+				os.Exit(1)
+			default:
+				fmt.Printf("Can't create the Consumer (Kafka error code %d)\n\tError: %v\n", ec, err)
+				os.Exit(1)
+			}
+		} else {
+			// It's not a kafka.Error
+			fmt.Printf("There's a generic error creating the Consumer! %v", err.Error())
+			os.Exit(1)
+		}
+
+	}
+	fmt.Printf("Created Consumer %v\n", cm)
+
+	// Subscribe to the topic
+	if err := c.Subscribe(*kafkaTopic, nil); err != nil {
+		fmt.Printf("There was an error subscribing to the topic :\n\t%v\n", err)
+		os.Exit(1)
+	}
+
+	run := true
+
+	for run {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+
+			ev := c.Poll(1000)
+			if ev == nil {
+				// the Poll timed out and we got nothing'
+				fmt.Printf("……\n")
+				continue
+			}
+			// The poll pulled an event, let's now
+			// look at the type of Event we've received
+			switch e := ev.(type) {
+
+			case *kafka.Message:
+				// It's a message
+				km := ev.(*kafka.Message)
+				cronJob := cronjob{}
+				fmt.Printf("✅ Message '%v' received from topic '%v' (partition %d at offset %d) key %v\n",
+					string(km.Value),
+					string(*km.TopicPartition.Topic),
+					km.TopicPartition.Partition,
+					km.TopicPartition.Offset,
+					string(km.Key))
+				err := json.Unmarshal(km.Value, &cronJob)
+				if err != nil {
+					fmt.Println(err)
+				}
+				execJob(cronJob.Command, cronJob.Args)
+
+			case kafka.Error:
+				// It's an error
+				em := ev.(kafka.Error)
+				fmt.Printf("Caught an error:\n\t%v\n", em)
+				if e.Code() == kafka.ErrAllBrokersDown {
+					run = false
+				}
+			default:
+				// It's not anything we were expecting
+				fmt.Printf("Got an event that's not a Message, Error, or PartitionEOF 👻\n\t%v\n", ev)
+
+			}
+		}
+	}
+	fmt.Printf("👋 … and we're done. Closing the consumer and exiting.\n")
+
+	// Now we can exit
+	c.Close()
+}
+
+func execJob(command string, args []string) {
+	cmd := exec.Command(command, args...)
+	cmd.Stderr = os.Stderr
+	stdout, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("Command Successfully Executed")
+	fmt.Println(string(stdout))
+}
--- kafka-cron/producer/producer.go
+++ kafka-cron/producer/producer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -73,6 +74,7 @@ func main() {
 						string(*ev.TopicPartition.Topic),
 						ev.TopicPartition.Partition,
 						ev.TopicPartition.Offset)
+					fmt.Println(ev.TopicPartition)
 				}
 			case kafka.Error:
 				// It's an error
@@ -121,4 +123,48 @@ func main() {
 	}
 	// Now we can exit
 	p.Close()
+}
+
+// CreateTopic creates a topic using the Admin Client API
+func CreateTopic(p *kafka.Producer, topic string) {
+
+	a, err := kafka.NewAdminClientFromProducer(p)
+	if err != nil {
+		fmt.Printf("Failed to create new admin client from producer: %s", err)
+		os.Exit(1)
+	}
+	// Contexts are used to abort or limit the amount of time
+	// the Admin call blocks waiting for a result.
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	// Create topics on cluster.
+	// Set Admin options to wait up to 60s for the operation to finish on the remote cluster
+	maxDur, err := time.ParseDuration("60s")
+	if err != nil {
+		fmt.Printf("ParseDuration(60s): %s", err)
+		os.Exit(1)
+	}
+	results, err := a.CreateTopics(
+		ctx,
+		// Multiple topics can be created simultaneously
+		// by providing more TopicSpecification structs here.
+		[]kafka.TopicSpecification{{
+			Topic:             topic,
+			NumPartitions:     2,
+			ReplicationFactor: 1}},
+		// Admin options
+		kafka.SetAdminOperationTimeout(maxDur))
+	if err != nil {
+		fmt.Printf("Admin Client request error: %v\n", err)
+		os.Exit(1)
+	}
+	for _, result := range results {
+		if result.Error.Code() != kafka.ErrNoError && result.Error.Code() != kafka.ErrTopicAlreadyExists {
+			fmt.Printf("Failed to create topic: %v\n", result.Error)
+			os.Exit(1)
+		}
+		fmt.Printf("%v\n", result)
+	}
+	a.Close()
+
 }
